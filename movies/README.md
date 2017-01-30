056c1eb8dc457a24be557d292ca313426cba6151
Aliciawyse <aliciawyse@gmail.com>
2017-01-29 21:45:10 -0500

    Updated to meet PEP8 standards

--- movies/README.md
+++ movies/README.md
@@ -1,3 +1,12 @@
-Entertainment-center.py, media.py and fresh_tomatoes.py build a movie website with all of my favorite movies. If you click on any one of them, its trailer will play. 
+# Movie Trailer Website
 
-No installation is required, other than having Python on your computer. Simply download the files (within the same directory), open entertainment-center.py and run the program.
+##What does this project do?
+All together, entertainment-center.py, media.py and fresh_tomatoes.py build a movie website with all of my favorite movies. If you click on any one of them, its trailer will play. 
+
+In media.py the class Movie is defined. In entertainment-center.py, multiple instances of the class Movie are created. Fresh_tomatoes.py is code provided by Udacity that creates a webpage where you can view your favorite movies; if you click on any of these movies you can view its trailer. 
+
+##How you can begin to use this project?
+You will need Python 2 to run this code. Simply download the files (within the same directory), open entertainment-center.py and run the program.
+
+
+
--- movies/entertainment-center.py
+++ movies/entertainment-center.py
@@ -1,35 +1,45 @@
-#Use the contents of media.py & fresh_tomatoes files
+# Use the contents of media.py & fresh_tomatoes files
 import fresh_tomatoes
 import media
 
-#Create six instances of the class Movie
-hidden_figures = media.Movie("Hidden Figures", "Black women working on one of the greatest missions at NASA",
-                             "https://upload.wikimedia.org/wikipedia/en/4/4f/The_official_poster_for_the_film_Hidden_Figures%2C_2016.jpg",
+# Create six instances of the class Movie
+hidden_figures = media.Movie("Hidden Figures",
+                             "Black women working on one of the \
+                             greatest missions at NASA",
+                             "https://upload.wikimedia.org/wikipedia/en/4/4f/ \
+                             The_official_poster_for_the_\
+                             film_Hidden_Figures%2C_2016.jpg",
                              "https://www.youtube.com/watch?v=RK8xHq6dfAo")
 
 arrival = media.Movie('Arrival', 'A linguistics expert talks to aliens',
-                      'https://upload.wikimedia.org/wikipedia/en/d/df/Arrival%2C_Movie_Poster.jpg',
+                      'https://upload.wikimedia.org/wikipedia/en/d/df/ \
+                       Arrival%2C_Movie_Poster.jpg',
                       'https://www.youtube.com/watch?v=tFMo3UJ4B4g')
 
 robo_cop = media.Movie('Robo Cop', 'A human cop becomes a robot cop',
-                       'https://upload.wikimedia.org/wikipedia/en/1/16/RoboCop_%281987%29_theatrical_poster.jpg',
+                       'https://upload.wikimedia.org/wikipedia/en/1/16/ \
+                        RoboCop_%281987%29_theatrical_poster.jpg',
                        'https://www.youtube.com/watch?v=FnRJ0r0eivk')
 
 martian = media.Movie('The Martian', 'A man gets left behind on mars.',
-                      'https://upload.wikimedia.org/wikipedia/en/thumb/c/cd/The_Martian_film_poster.jpg/220px-The_Martian_film_poster.jpg',
+                      'https://upload.wikimedia.org/wikipedia/en/thumb/c/cd/\
+                      The_Martian_film_poster.jpg/220px-The_\
+                      Martian_film_poster.jpg',
                       'https://www.youtube.com/watch?v=tFMo3UJ4B4g')
 
-mummy = media.Movie('The Mummy', 'An ancient egyptian priest is accidently resurrected',
-                    'https://upload.wikimedia.org/wikipedia/en/thumb/6/68/The_mummy.jpg/220px-The_mummy.jpg',
+mummy = media.Movie('The Mummy', 'An ancient egyptian priest is \
+                    accidently resurrected',
+                    'https://upload.wikimedia.org/wikipedia \
+                    /en/thumb/6/68/The_mummy.jpg/220px-The_mummy.jpg',
                     'https://www.youtube.com/watch?v=h3ptPtxWJRs')
 
 fences = media.Movie('Fences', 'A father misses a lifetime of opportunities',
-                     'https://upload.wikimedia.org/wikipedia/en/0/0d/Fences_%28film%29.png',
+                     'https://upload.wikimedia.org/wikipedia/en/\
+                     0/0d/Fences_%28film%29.png',
                      'https://www.youtube.com/watch?v=a2m6Jvp0bUw')
 
-#List of movies 
+# List of movies
 movies = [hidden_figures, arrival, robo_cop, martian, mummy, fences]
 
-#Use list of movies as the arguement to open_movies_page function 
+# Use list of movies as the arguement to open_movies_page function
 fresh_tomatoes.open_movies_page(movies)
-
